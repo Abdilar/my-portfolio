a08f583dd39f322b1b8c0f99051d07e89b21873d
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-01 23:28:00 +0430

    create contact page

---
+++ src/pages/Contact/Contact.page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <section>
+          <h1>Contact Page</h1>
+        </section>
+    );
+  }
+}
