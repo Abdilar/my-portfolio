85c7c7c4bf18717f2fec23123138ef1a68de664f
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2025-03-23 02:09:55 +0330

    feat: bad

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,12 @@
 module.exports = { 
   extends: ["@commitlint/config-conventional"],
   rule: {
+    'header-max-length': [2, 'always', 200],
+    'header-min-length': [2, 'always', 20],
     'body-max-line-length': [2, 'always', 200],
-    'body-min-length': [2, 'always', 10],
+    'body-min-length': [2, 'always', 20],
     'subject-max-length': [2, 'always', 50],
-    'subject-min-length': [2, 'always', 20]
+    'subject-min-length': [2, 'always', 20],
   }
 
 };
