69c4c74f4664601b4281a2fa11c5ec19ae5c937f
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2025-03-23 00:26:54 +0330

    delete the project

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouting from './app.route';
-
-import 'normalize.css/normalize.css';
-import './assets/styles/global.scss';
-
-ReactDOM.render(<AppRouting />, document.getElementById('app'));
